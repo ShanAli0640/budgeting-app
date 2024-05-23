0b771d2541540ed37d63fadb1d7a59cc72817c88
RogerDewald <daniel.r.dewald@gmail.com>
2024-05-23 15:08:19 -0500

    integrated auth0

--- wealth-wave/src/app/(main)/login/page.tsx
+++ wealth-wave/src/app/(main)/login/page.tsx
@@ -1,6 +1,13 @@
-
-export default function Login(){
-    return(
-        <p> howdy </p>
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+export default function Login() {
+    const { user, error, isLoading } = useUser();
+    return (
+        <>
+            <a href="/api/auth/login">
+                yo
+            </a>
+            <p> howdy </p>
+        </>
     )
 }
